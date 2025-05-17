74fce6a63745797001692874db9b82d735dc88be
yannik131 <yannik131@web.de>
2025-05-17 12:32:18 +0200

    pdf generation script

---
+++ 2/yannik/generate_pdf.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+enscript \
+    --header='$n - Page $% of $= || Moritz Lunk, bt712482 | Yannik Schroeder, bt722248 | Justin Stoll, bt709742' \
+    --header-font='Helvetica-Bold10' \
+    --line-numbers \
+    --color \
+    --highlight=c \
+    -f Courier8 \
+    -T 4 \
+    1_1.c WuerfelRennen.c 1_3_cond.c \
+    -o output.ps
+
+ps2pdf output.ps output.pdf
