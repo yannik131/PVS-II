37a43e593d41a5eded78c07b253f1cc95d3bef8b
Justin Stoll <109173213+SyMyX@users.noreply.github.com>
2025-06-07 13:21:41 +0200

    Add 4/justin/A4

---
+++ 4/justin/A4/A4_4.java
@@ -0,0 +1,24 @@
+import java.lang.Thread;
+
+class MyThread extends Thread {
+    private int n;
+
+    public MyThread(int n) {
+        this.n = n;
+    }
+
+    public void run() {
+        System.out.println("Hallo ich bin Thread " + n);
+    }
+}
+
+class A4_4 {
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        for (int i = 0; i < n; i++) {
+            MyThread thread = new MyThread(i + 1);
+            thread.run();
+        }
+        System.out.println("Hallo von Main..ich have " + n + " Thread erzeugt");
+    }
+}
