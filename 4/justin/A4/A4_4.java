ba8215c60663932125f752ec307cf67ad881efc8
Justin Stoll <109173213+SyMyX@users.noreply.github.com>
2025-06-12 10:38:27 +0200

    Fixed 4/justin/A4/A4_4.java to start and join threads

--- 4/justin/A4/A4_4.java
+++ 4/justin/A4/A4_4.java
@@ -15,9 +15,16 @@ class MyThread extends Thread {
 class A4_4 {
     public static void main(String[] args) {
         int n = Integer.parseInt(args[0]);
+        MyThread[] threads = new MyThread[n];
         for (int i = 0; i < n; i++) {
-            MyThread thread = new MyThread(i + 1);
-            thread.run();
+            threads[i] = new MyThread(i + 1);
+            threads[i].start();
+        }
+        for (int i = 0; i < n; i++) {
+            try {
+                threads[i].join();
+            }
+            catch (Exception e) { }
         }
         System.out.println("Hallo von Main..ich have " + n + " Thread erzeugt");
     }
